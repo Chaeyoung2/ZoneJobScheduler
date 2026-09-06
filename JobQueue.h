a76f22e066b0f9b8f45d210c4d42e2a6a7229394
Chaeyoung2 <43426737+Chaeyoung2@users.noreply.github.com>
2026-09-06 17:38:46 +0900

    shutdown 구현 - is_shutdown 갱신을 mutex로 보호해서 lost wakeup 방지 + unique_lock으로 lock 조기 해제하여 불필요한 블로킹 방지

--- JobQueue.h
+++ JobQueue.h
@@ -6,7 +6,14 @@ class Job;
 class JobQueue {
 
 public:
-	void shut_down();
+	void shut_down()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		is_shutdown = true;
+		lock.unlock();
+
+		cv.notify_all();
+	}
 	void push(const Job& job);
 	bool pop(Job& job);
 
