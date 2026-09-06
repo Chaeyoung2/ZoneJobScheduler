b317c5a06ae7a025e44e0f82a89e992dc006f6a8
Chaeyoung2 <43426737+Chaeyoung2@users.noreply.github.com>
2026-09-06 17:26:30 +0900

    JobQueue 초안 작성

--- JobQueue.h
+++ JobQueue.h
@@ -1,48 +1,18 @@
 #pragma once
 #include <queue>
 #include <mutex>
-#include <condition_variable>
-#include <functional>
 
-class JobQueue 
-{
+class Job;
+class JobQueue {
+
 public:
-    using Job = std::function<void()>;
-
-    void push(Job job) 
-    {
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            m_jobs.push(std::move(job));
-        }
-        m_cv.notify_one();
-    }
-
-    bool pop(Job& out) 
-    {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_cv.wait(lock, [this] { return !m_jobs.empty() || m_shutdown; });
-
-        if (m_jobs.empty()) {
-            return false;
-        }
-        out = std::move(m_jobs.front());
-        m_jobs.pop();
-        return true;
-    }
-
-    void shutdown() 
-    {
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            m_shutdown = true;
-        }
-        m_cv.notify_all();
-    }
+	void shut_down();
+	void push(const Job& job);
+	bool pop(Job& job);
 
 private:
-    std::queue<Job> m_jobs;
-    std::mutex m_mutex;
-    std::condition_variable m_cv;
-    bool m_shutdown = false;
+	std::mutex mutex;
+	std::condition_variable cv;
+	std::queue job_queue;
+	bool is_shutdown;
 };
