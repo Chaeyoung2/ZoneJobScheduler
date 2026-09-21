41ff8e330813e26c77353f0aec8ce2a5da01c7ba
Chaeyoung2 <43426737+Chaeyoung2@users.noreply.github.com>
2026-09-21 21:05:30 +0900

    fix(actor): 음수 데미지로 HP가 증가할 수 있는 문제 수정 및 코드 정리

--- include/Actor.h
+++ include/Actor.h
@@ -4,13 +4,11 @@ class Actor
 {
 public:
     Actor();
-    Actor(int initialMaxHp, int initialHp);
 
     void takeDamage(int damage);
     int getHp() const;
 
 private:
-    const int m_maxHp = 100;
     int m_hp = 100;
 };
 
--- src/Actor.cpp
+++ src/Actor.cpp
@@ -4,18 +4,14 @@
 
 Actor::Actor() = default;
 
-Actor::Actor(int initialMaxHp, int initialHp)
-    : m_maxHp(initialMaxHp),
-      m_hp(initialHp)
-{
-	assert(initialMaxHp >= 0);
-	assert(initialHp >= 0);
-	assert(initialHp <= initialMaxHp);
-}
-
 void Actor::takeDamage(int damage)
 {
 	assert(damage >= 0);
+
+    if (damage <= 0)
+    {
+        return;
+    }
 
     if (damage > m_hp)
     {
